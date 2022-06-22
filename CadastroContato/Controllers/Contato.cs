71828ba82709f8bfdae2a7d42e88c7bcbd27f3e6
Felipe Muniz <lipe.baterra@gmail.com>
2022-06-21 23:31:37 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroContato/Controllers/Contato.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroContato.Controllers
+{
+    public class Contato : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+        public IActionResult DeletarConfirmar()
+        {
+            return View();
+        }        
+    }
+}
---
+++ CadastroContato/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using CadastroContato.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CadastroContato.Controllers
+{
+    public class HomeController : Controller
+    { 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
