e425f3eb568bcf958929272ba31511e4943428e7
Felipe Muniz <lipe.baterra@gmail.com>
2022-07-02 11:56:58 -0300

    Criando alterarção de senha do usuario

---
+++ CadastroContato/Controllers/AlterarSenhaController.cs
@@ -0,0 +1,32 @@
+﻿using CadastroContato.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CadastroContato.Controllers
+{
+    public class AlterarSenhaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(AlterarSenhaModel alterarSenhaModel)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    TempData["MensagemSucesso"] = "Senha Alterada com sucesso!";
+                    return View("Index", alterarSenhaModel);
+                }
+                return View("Index", alterarSenhaModel);
+            }
+            catch(Exception erro)
+            {
+                TempData["MensagemErro"] = $"Nao conseguimos alterar a senha, tente novamente, detalhe do erro: {erro.Message}";
+                return View("Index", alterarSenhaModel);
+            }
+        }
+    }
+}
---
+++ CadastroContato/Models/AlterarSenhaModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroContato.Models
+{
+    public class AlterarSenhaModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Digite a senha atual")]
+        public string SenhaAtual { get; set; }
+
+        [Required(ErrorMessage = "Digite a nova senha")]
+        public string NovaSenha { get; set; }
+
+        [Required(ErrorMessage = "Confirme a nova senha")]
+        [Compare("NovaSenha", ErrorMessage ="A senha e a confirmação deverá ser igual")]
+        public string ConfirmarSenha { get; set; }  
+    }
+}
